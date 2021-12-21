703645c6feeee9b266380d70af8207c4944be52e
Pranjal Verma <40911241+pranjalv01@users.noreply.github.com>
2021-12-21 18:03:47 +0530

    Delete multi-stakeholder-ml/policies directory

--- multi-stakeholder-ml/policies/dataowner/get_public_credential.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export SCONE_CLI_CONFIG="~/.cas/dataowner-config.json"
-
-#Public Key Hash: 3giGD5tqjHaxB3xELKGK8Eee2ny8v3zddfcjaMBLDTrygn4x4X
-/opt/scone/bin/rust-cli self show|awk -F "Public Key Hash: " '{print $2}'
